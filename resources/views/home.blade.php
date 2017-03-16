9d32b8f524d0b367f2ce726fae9daf883c5737e8
Dennis Smink <dennissmink@gmail.com>
2017-03-16 09:24:23 +0100

    Removed copyright

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,8 +9,6 @@
 
         <div class="panel-body">
             Welcome to Laravel Newsletter, an application where you can send easily a newsletter to your subscribers.
-            <br><br>
-            Made with <i class="fa fa-heart text-danger"></i> by <a href="https://twitter.com/NathanGeerinck">@NathanGeerinck</a>
         </div>
     </div>
 </div>
